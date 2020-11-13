6090eb2b6b32ad7ddfd387caa80632f374f8fc9b
Hua Luo <hluo@lifesize.com>
2020-11-13 16:46:58 -0600

    make the starter works

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 #start the service
 cd /app/
-gunicorn -w 1
-	-k uvicorn.workers.UvicornWorker snowflake.main:app
+gunicorn -w 1 \
+	-k uvicorn.workers.UvicornWorker starter.main:app \
 	-b 0.0.0.0:8000
